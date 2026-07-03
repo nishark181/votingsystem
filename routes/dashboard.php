d1294d521d4013366de80c14dfc4b6dfaf83cdba
Nisha moni <nm589612@gmail.com>
2026-07-03 20:01:10 +0600

    Add files via upload

---
+++ routes/dashboard.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Online Voting System  - Dashboard</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>logout</button>
+        <h1>Online Voting </h1>
+        <hr>
+        <div id="profile"></div>
+        <div id="Group"></div>
+    </body>
+</html>
